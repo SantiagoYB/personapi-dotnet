5f0c06a07710a82d18f7c4922ac96359fcfd3f9f
SantiagoYB <98602870+SantiagoYB@users.noreply.github.com>
2024-10-07 17:34:24 -0500

    Cambio en el personaDbContext

--- personapi-dotnet/Models/Entities/PersonaDbContext.cs
+++ personapi-dotnet/Models/Entities/PersonaDbContext.cs
@@ -25,8 +25,8 @@ public partial class PersonaDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=persona_db;Trusted_Connection=True;TrustServerCertificate=true");
-
+        => optionsBuilder.UseSqlServer("Server=personapi-dotnet-db-1;Database=persona_db;Trusted_Connection=True;TrustServerCertificate=true");
+    //aca se debe cambiar para que no peuge al mismo contenedor!!!!!
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Estudio>(entity =>
